1ebf68ed8bef400fd00647f84297232a3ff97d1a
gursharankangz@icloud.com <gursharankangz@icloud.com>
2021-06-26 16:14:38 +0300

    Add project files.

---
+++ SalonManagement_Gursharn/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SalonManagement_Gursharn.Models;
+
+namespace SalonManagement_Gursharn.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Staff> Staffs { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
+    }
+}
